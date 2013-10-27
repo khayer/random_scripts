1d62d20a2e272a2611061c2a764500c768cc4736
khayer <katharinaehayer@gmail.com>
2013-10-27 19:41:56 -0400

    hitting the pause button

--- merge_xls_sheets.rb
+++ merge_xls_sheets.rb
@@ -66,9 +66,9 @@ def run_bl2seq(gene1,gene2)
   name2 = `grep #{gene2} #{$index_file}`.split(" ")[0].delete(">")
   #gene1 = "hg19_refGene_#{gene1}"
   #gene2 = "hg19_refGene_#{gene2}"
-  gene1 = name1.split("|").join("\|")
+  gene1 = name1.split("|").join("\\|")
   $logger.debug("gene1 #{gene1}")
-  gene2 = name1.split("|").join("\|")
+  gene2 = name1.split("|").join("\\|")
   `samtools faidx #{$index_file} #{gene1} > tmp1.fa`
   `samtools faidx #{$index_file} #{gene2} > tmp2.fa`
   out = `bl2seq -F F -i tmp1.fa -j tmp2.fa -p blastn -D 1`
