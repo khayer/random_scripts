4d070fc3daf0dc9e0404a5eff6afa5c27f222ada
khayer <katharinaehayer@gmail.com>
2014-01-03 10:32:54 -0500

    hitting the pause button

--- junctions.rb
+++ junctions.rb
@@ -79,7 +79,7 @@ def read_index(fai_file)
     chr,length,start = line.split("\t")
     length = length.to_i
     start = start.to_i
-    bias = length*5
+    bias = length/2
     fai_index[chr] = {:start => start, :stop => start+length+bias-1}
   end
   fai_index
