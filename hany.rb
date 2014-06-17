e6ad40478c0c00f943433be6a38427f729ad1aeb
Katharina Hayer <katharinaehayer@gmail.com>
2014-06-17 10:05:41 -0400

    hitting the pause button

--- hany.rb
+++ hany.rb
@@ -333,7 +333,7 @@ def read_trans(trans_file)
   pair = []
   File.open(trans_file).each do |line|
     line.chomp!
-    next unless line =~ /NH:i:1\s/
+    #next unless line =~ /NH:i:1\s/
     fields = line.split("\t")
     next if fields[2] == "*"
     name ||= fields[0]
